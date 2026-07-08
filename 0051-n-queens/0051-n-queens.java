46a36a54c110648a032521b582a1691fe4582676
Yash Bhardwaj <yashb18.04@gmail.com>
2026-07-08 20:27:16 +0530

    Time: 2 ms (83.55%), Space: 46.3 MB (92.06%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,23 +1,22 @@
 class Solution {
-    public boolean isSafe(char[][] board,int row, int col, int n) {
-        //Horizontal
-        for( int j=0 ; j<n ; j++){
-            if(board[row][j] == 'Q') return false;
+    public boolean isSafe(char[][] board, int row, int col, int n) {
+
+    // Check only previous rows in the same column
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q')
+                return false;
         }
 
-        //vertical
-        for( int i=0 ; i<n ; i++ ){
-            if(board[i][col] == 'Q') return false;
+        // Upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
         }
 
-        // left diagonal
-        for (int i = row , j = col ; i >= 0 && j >=0 ; i--,j--){
-            if(board[i][j] == 'Q') return false;
-        }
-
-        //right diagonal
-        for (int i = row , j = col ; i >= 0 && j < n ; i--,j++) {
-            if(board[i][j] == 'Q') return false;
+        // Upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
         }
 
         return true;
