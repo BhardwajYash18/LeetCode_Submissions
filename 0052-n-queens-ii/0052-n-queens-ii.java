ea2ec0ffbe45e8ad2e23c376b5507003949e922b
Yash Bhardwaj <yashb18.04@gmail.com>
2026-07-08 20:33:25 +0530

    Time: 2 ms (64.16%), Space: 42.3 MB (29.87%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,58 @@
+class Solution {
+    int count = 0;
+    public boolean isSafe(char[][] board, int row, int col, int n) {
+
+    // Check only previous rows in the same column
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // Upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // Upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+    public void nQueen(char[][] board, int row, int n) {
+        if (row == n){
+            count++;
+            return;
+        }
+
+        for (int j = 0 ; j < n ; j++) {
+            if(isSafe(board,row,j,n)){
+                board[row][j] = 'Q';
+                nQueen(board, row+1, n);
+                board[row][j] = '.';
+            }
+
+        }
+    }
+
+    public int solve(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < n ; j++) {
+                board[i][j] = '.';
+            }
+        }
+        count = 0;
+        nQueen(board, 0, n);
+
+        return count;
+    }
+
+    
+    public int totalNQueens(int n) {
+        return solve(n);
+    }
+}
