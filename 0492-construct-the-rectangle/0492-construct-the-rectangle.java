e2e8f2a1491ad4203dfda784fdaa8f1efa9e6219
Yash Bhardwaj <yashb18.04@gmail.com>
2026-08-21 20:03:37 +0530

    Time: 0 ms (100%), Space: 42.5 MB (48.75%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.java
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] constructRectangle(int area) {
-        int wMax = (int)Math.sqrt(area);
-        int[] arr = new int[2];
-        int min = Integer.MAX_VALUE;
-        int w = 1;
-        while ( w <= wMax) {
-            if (area % w == 0) {
-                int l = area / w;
-                if (Math.abs(l-w) <= min){
-                    arr[0] = l;
-                    arr[1] = w;
-                }
-            }
-            w++; 
-        } 
-        return arr;
+
+        int width = (int) Math.sqrt(area);
+
+        while (area % width != 0) {
+            width--;
+        }
+
+        int length = area / width;
+
+        return new int[]{length, width};
     }
 }
