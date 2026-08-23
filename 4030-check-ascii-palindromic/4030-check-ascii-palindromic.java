036be79709278aabe0e49bca3af74215cb3d8626
Yash Bhardwaj <yashb18.04@gmail.com>
2026-08-23 22:24:20 +0530

    Time: 13 ms (30.53%), Space: 46.5 MB (37.1%) - LeetHub

--- 4030-check-ascii-palindromic/4030-check-ascii-palindromic.java
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.java
@@ -5,11 +5,8 @@ class Solution {
             int num = (int) c;
             sb.append(String.format("%8s", Integer.toBinaryString(num)).replace(' ', '0'));
         } 
-        return pallindrome(sb.toString());
-    }
-    private boolean pallindrome(String s) {
-        for (int i = 0; i < s.length()/2 ; i++) {
-            if (s.charAt(i) != s.charAt(s.length()-1-i))
+        for (int i = 0; i < sb.length()/2 ; i++) {
+            if (sb.charAt(i) != sb.charAt(sb.length()-1-i))
                 return false;
         }
         return true;
