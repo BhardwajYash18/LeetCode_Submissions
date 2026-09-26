047939f36852278f0375da9b840a69a0a9a77924
Yash Bhardwaj <yashb18.04@gmail.com>
2026-09-26 11:49:33 +0530

    Time: 2 ms (25.02%), Space: 47.2 MB (19.52%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new ArrayList<>(Arrays.asList(intervals));
-        result.add(newInterval);
-        Collections.sort(result, (a,b) -> Integer.compare(a[0],b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int i = 0;
 
-        List<int[]> answer = new ArrayList<>();
-        int[] curr = result.get(0);
+        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+            merged.add(intervals[i]);
+            i++;
+        }
 
-        for (int i = 1; i < result.size(); i++) {
-            int[] interval = result.get(i);
-            if (curr[1] >= interval[0])
-                curr[1] = Math.max(curr[1], interval[1]);
-            else {
-                answer.add(curr);
-                curr = interval;
-            }
-        } 
-        answer.add(curr);
-        return answer.toArray(new int[answer.size()][]);
+        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+            newInterval = new int[] {
+                Math.min(newInterval[0], intervals[i][0]),
+                Math.max(newInterval[1], intervals[i][1])
+            };
+            i++;
+        }
+        merged.add(newInterval);
+        while (i < intervals.length) {
+            merged.add(intervals[i]);
+            i++;
+        }
+
+        return merged.toArray(new int[merged.size()][]);
     }
 }
